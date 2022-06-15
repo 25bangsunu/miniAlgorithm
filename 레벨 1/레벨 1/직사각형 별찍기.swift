4b59123d2e9150ecd49ead7ca8231ef0bde7c924
25bangsunu <onlyliveonejustdoit@gmail.com>
2022-06-15 23:26:32 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ 레벨 1/레벨 1/직사각형 별찍기.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  직사각형 별찍기
+//
+//  Created by 방선우 on 2022/06/15.
+//
+
+import Foundation
+
+//MARK: 직사각형 별찍기
+
+//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//let (a, b) = (n[0], n[1])
+//
+//var arr: [String] = []
+//
+//for _ in 1...n[0] {
+//    arr.append("*")
+//}
+//
+//for _ in 1...n[1] {
+//    print(arr.joined())
+//}
+//
