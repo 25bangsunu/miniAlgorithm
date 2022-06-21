6cbef39f9baa1142fcdfbd3d86c8f44be15868c3
25bangsunu <onlyliveonejustdoit@gmail.com>
2022-06-21 18:13:09 +0900

    평균 구하기

--- 레벨 1/레벨 1/main.swift
+++ 레벨 1/레벨 1/main.swift
@@ -1,5 +1,5 @@
 //
-//  main.swift
+//  File.swift
 //  레벨 1
 //
 //  Created by 방선우 on 2022/06/21.
@@ -7,16 +7,8 @@
 
 import Foundation
 
-//MARK: 하샤드 수
+//MARK: 평균구하기
 
-func solution(_ x:Int) -> Bool {
-    let d: [Int] = [10000, 1000, 100, 10]
-    var result: Int = x
-    var a = 0
-    
-    for i in d {
-        a += result / i
-        result = x % i
-    }
-    return x % (a + result) == 0
+func solution(_ arr:[Int]) -> Double {
+    return Double(arr.reduce(0, +)) / Double(arr.count)
 }
---
+++ 레벨 1/레벨 1/하샤드수.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  레벨 1
+//
+//  Created by 방선우 on 2022/06/21.
+//
+
+import Foundation
+
+//MARK: 하샤드 수
+
+//func solution(_ x:Int) -> Bool {
+//    let d: [Int] = [10000, 1000, 100, 10]
+//    var result: Int = x
+//    var a = 0
+//
+//    for i in d {
+//        a += result / i
+//        result = x % i
+//    }
+//    return x % (a + result) == 0
+//}
+
+func solution(_ x:Int) -> Bool {
+    return x % String(x).compactMap { Int(String($0)) }.reduce(0, +) == 0
+}
+
